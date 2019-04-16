10ce45ce0f0ca0a067f6c9048e256444a0a98032
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-16 14:57:17 -0400

    fixed bug for the posts show in the #posts section of the ui

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,6 @@
 class UI {
     constructor() {
-        this.posts = document.querySelector('#posts');
+        this.post = document.querySelector('#posts');
         this.titleInput = document.querySelector('#title');
         this.bodyInput = document.querySelector('#body');
         this.idInput = document.querySelector('#id');
