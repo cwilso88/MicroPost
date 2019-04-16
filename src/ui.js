fdc02020a20822332c531deedb2c2e3fe6cb525f
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-16 14:45:07 -0400

    created showPosts function with a forEach

--- src/ui.js
+++ src/ui.js
@@ -8,5 +8,23 @@ class UI {
         this.forState = 'add';
     }
 
-    showPosts()
+    showPosts(posts) {
+        let output = '';
+
+        posts.forEach((post) => {
+            output += `
+            <div class= "card mb-3">
+                <div class= "card-body">
+                <h4 class= "card-title">${post.title}</h4>
+                <p class= "card-text">${post.body}</p>
+                </div>
+            </div>
+            `;
+        });
+
+        this.post.innerHTML = output;
+    }
 }
+
+
+export const ui = new UI();
