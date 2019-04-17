d6eeceaa0f4cc71cd3945aa166acf09815583ce0
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-17 06:46:28 -0400

    created the  function for deletePost for when the delete icon is clicked

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 
 // Get Post
 function getPosts() {
@@ -24,7 +27,7 @@ function submitPost() {
     title,
     body
   }
-
+  // Create Post
   http.post('http://localhost:3000/posts', data)
     .then(data => {
       ui.showAlert('Yipee! Post added', 'alert alert-success');
@@ -32,4 +35,21 @@ function submitPost() {
       getPosts();
     })
     .catch(err => console.log(err));
+}
+
+// Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')){
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-success')
+          ;
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
 }
